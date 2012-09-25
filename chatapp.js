0df458179ad9c941d1ca512fff8126104f162c92
Ernest S <ernest.s@multunus.com>
2012-09-26 02:24:25 +0530

    NOTWORKING:add chatrooms and users

--- chatapp.js
+++ chatapp.js
@@ -1,9 +1,57 @@
 Messages = new Meteor.Collection('messages');
+Users = new Meteor.Collection('users');
+ChatRooms = new Meteor.Collection('chatRooms');
 
-if (Meteor.is_client) {
+SESSION_IDENTIFIER_FOR_USER = "currentUser"
+
+
+if (Meteor.isClient) {
   Template.list.messages = function(){
     return Messages.find({});
   };
+
+  Template.userLoginTemplate.events = {
+    'click input.username-button' : function(){
+      var userName = $('input.username').val();
+      //Log the user in
+      
+      user = Users.findOne({username: userName});
+
+      HELPER.clog("INFO",userName);
+      HELPER.clog("INFO",user);
+
+      if(!_.isNull(user) && !_.isUndefined(user)){
+        HELPER.clog('info', "in true");
+        Session.set(SESSION_IDENTIFIER_FOR_USER, userName);
+      }else{
+        HELPER.clog('info', "in false");
+        Users.insert({username: userName}, function(errorObject, userId){
+          if(errorObject){
+            HELPER.clog('info', "error happened");
+            clog("ERROR:",errorObject);
+          }else{
+            HELPER.clog('info', "good happened");
+            HELPER.clog("INFO:",userId);
+          }          
+        });
+        Session.set(SESSION_IDENTIFIER_FOR_USER, userName);
+      }
+      // Hide the login form
+      $('.step1').addClass('hidden');
+    }
+  };
+
+  Template.chatRoomsListTemplate.chatRooms = function(){
+    return ChatRooms.find({});
+  };
+
+  Template.chatRoomTemplate.events = {
+    'click span.chat-room-name' : function(){
+      var chatRoomName = $('span.chat-room-name').text();
+      chatRoom = ChatRooms.findOne({name :  chatRoomName });      
+    }
+  };
+
   Template.newMessageTemplate.events = {
     'click input.go-button' :  function(){
       var messageTextInputField = $('input.message-text');
@@ -14,18 +62,15 @@ if (Meteor.is_client) {
     }
   };
 
-  Template.hello.events = {
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  };
+ 
 }
 
-if (Meteor.is_server) {
+if (Meteor.isServer) {
   Meteor.startup(function () {    
     // code to run on server at startup
+    if (Users.find().count() === 0) {
+      Users.insert({username:"ernest"});
+    }
     if (Messages.find().count() === 0) {
       var initial_messages = ["Ada Lovelace",
                    "Grace Hopper",
@@ -37,4 +82,12 @@ if (Meteor.is_server) {
         Messages.insert({message : initial_messages[i]});
     }
   });
+}
+
+HELPER = {
+  clog : function(tag,message){
+    console.log('******'+ tag + '*************');
+    console.log(message);
+    console.log("********************");
+  }
 }
